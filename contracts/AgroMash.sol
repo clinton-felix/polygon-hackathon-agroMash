e33ecd143ec619b108563842756d3c5861d57f30
Clinton <archimedesfelix1@gmail.com>
2022-12-04 22:59:30 +0100

    env

--- contracts/AgroMash.sol
+++ contracts/AgroMash.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.0;
 
 // INTERNAL IMPORTS FROM OPENZEPPELIN
 import "@openzeppelin/contracts/utils/Counters.sol";
