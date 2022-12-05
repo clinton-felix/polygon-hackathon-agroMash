cb52858ac0ec5e693c37ef314eb742b5466c5200
Clinton <archimedesfelix1@gmail.com>
2022-12-05 01:55:42 +0100

    ABI, ADDRESS

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,47 +3,47 @@ import { useMoralis } from "react-moralis";
 import { useEffect } from "react";
 
 const Navigation = ({ account, setAccount }) => {
-  const {
-    enableWeb3,
-    //account,
-    isWeb3Enabled,
-    Moralis,
-    deactivateWeb3,
-    isWeb3EnableLoading,
-  } = useMoralis();
+  //   const {
+  //     enableWeb3,
+  //     account,
+  //     isWeb3Enabled,
+  //     Moralis,
+  //     deactivateWeb3,
+  //     isWeb3EnableLoading,
+  //   } = useMoralis();
 
-  // use effect that checks to see if we are disconnected
-  // and if we are, prompts for a connection
-  useEffect(() => {
-    // check if the browser is already connected, and return if True
-    if (isWeb3Enabled) return;
-    if (typeof window !== undefined) {
-      if (window.localStorage.getItem("connected")) {
-        enableWeb3();
-      }
-    }
-  }, [isWeb3Enabled]);
+  //   // use effect that checks to see if we are disconnected
+  //   // and if we are, prompts for a connection
+  //   useEffect(() => {
+  //     // check if the browser is already connected, and return if True
+  //     if (isWeb3Enabled) return;
+  //     if (typeof window !== undefined) {
+  //       if (window.localStorage.getItem("connected")) {
+  //         enableWeb3();
+  //       }
+  //     }
+  //   }, [isWeb3Enabled]);
 
-  // useEffect here will check to see if we are connected and
-  useEffect(() => {
-    Moralis.onAccountChanged((account) => {
-      console.log(`Account changed to ${account}`);
-      // if account == nul, we assume they have disconnected
-      if (account == null) {
-        window.localStorage.removeItem("connected");
-        deactivateWeb3();
-        console.log("Null account found");
-      }
-    });
-  }, []);
-
-  //   const connectHandler = async () => {
-  //     const accounts = await window.ethereum.request({
-  //       method: "eth_requestAccounts",
+  //   // useEffect here will check to see if we are connected and
+  //   useEffect(() => {
+  //     Moralis.onAccountChanged((account) => {
+  //       console.log(`Account changed to ${account}`);
+  //       // if account == nul, we assume they have disconnected
+  //       if (account == null) {
+  //         window.localStorage.removeItem("connected");
+  //         deactivateWeb3();
+  //         console.log("Null account found");
+  //       }
   //     });
-  //     setAccount(accounts[0]);
-  //     console.log(accounts[0]); // reading from the state
-  //   };
+  //   }, []);
+
+  const connectHandler = async () => {
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    setAccount(accounts[0]);
+    console.log(accounts[0]); // reading from the state
+  };
   return (
     <nav>
       <ul className="nav__links">
@@ -63,13 +63,14 @@ const Navigation = ({ account, setAccount }) => {
         <h1>AgroMash</h1>
       </div>
 
-      {account ? (
+      {/* {account ? (
         <div>
           Connected to: {account.slice(0, 6)}....
           {account.slice(account.length - 4)}
         </div>
       ) : (
         <button
+          className="nav__connect"
           onClick={async () => {
             // enable the web3 connection on click of the button
             // store a memory tracker in local storage so that the dapp remembers that user is connected
@@ -79,6 +80,15 @@ const Navigation = ({ account, setAccount }) => {
             }
           }}
           disabled={isWeb3EnableLoading}>
+          Connect
+        </button>
+      )} */}
+      {account ? (
+        <button type="button" className="nav__connect">
+          {account.slice(0, 6) + "..." + account.slice(38, 42)}
+        </button>
+      ) : (
+        <button type="button" className="nav__connect" onClick={connectHandler}>
           Connect
         </button>
       )}
